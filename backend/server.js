112d931dce8ade2a24973cbc5e69d7b32e0810e4
Rewant-1 <rewant.bhriguvanshi@gmail.com>
2025-05-27 13:32:37 +0530

    Added user routes Added notification model

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ app.use(express.urlencoded({extended:true}));
 import connectMongoDb from "./db/connectMongoDb.js";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 dotenv.config();
 app.use(cookieParser()); //to parse cookies
 app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 console.log(process.env.MONGO_URI);
 const PORT=process.env.PORT || 5000;
  //to parse urlencoded data
