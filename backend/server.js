7fb898a0535d531bc7b7dcf8cd4f275f2fd8790f
Rewant-1 <rewant.bhriguvanshi@gmail.com>
2025-05-28 13:18:24 +0530

    consistent naming of route files and paths

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";  
-import postRoutes from "./routes/post.routes.js";
+import postRoutes from "./routes/post.route.js";
  
 const app= express();
 app.use(express.json()); //to parse req.body
 app.use(express.urlencoded({extended:true}));
 import connectMongoDb from "./db/connectMongoDb.js";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
