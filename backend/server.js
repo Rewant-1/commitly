2acc10bd686eb9a6124e46de0de251e351188b34
Rewant-1 <rewant.bhriguvanshi@gmail.com>
2025-06-01 19:48:10 +0530

    Suggested users fetched

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
  
 const app= express();
-app.use(express.json()); //to parse req.body
+app.use(express.json({limit:"5mb"})); //to parse req.body
 app.use(express.urlencoded({extended:true}));
 import connectMongoDb from "./db/connectMongoDb.js";
 import dotenv from "dotenv";
